5d3b2aaaee71050dc417b54e38dbbb41b5d39840
Sweta <sharma2020sweta@gmail.com>
2021-02-16 15:08:04 +0500

    First Git Project

--- app/src/main/java/com/makevide/testgit/MainActivity.kt
+++ app/src/main/java/com/makevide/testgit/MainActivity.kt
@@ -5,6 +5,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        Toast.makeText(applicationContext,"HELLO ",Toast.LENGTH_LONG).show()
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
